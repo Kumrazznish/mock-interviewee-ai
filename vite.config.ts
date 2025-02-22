69e55527bb3ffd58036d69c1deea27ae48dcf5b7
Kumrazznish <147333622+Kumrazznish@users.noreply.github.com>
2025-02-22 08:29:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
 import path from "path";
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: "0.0.0.0", // Render को सर्वर एक्सपोज़ करने के लिए
+    port: process.env.PORT || 5173, // Render द्वारा दिए गए पोर्ट का उपयोग करें
+  },
+  preview: {
+    port: process.env.PORT || 5173, // Preview mode के लिए भी पोर्ट सेट करें
   },
 });
